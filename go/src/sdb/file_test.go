5a4eab6d6f5e8324fe92983b4a0aa3d99a0ff8cb
Snappy System <snappysystem@gmail.com>
2014-10-28 19:01:42 -0700

    Adding more test case for file.go

--- go/src/sdb/file_test.go
+++ go/src/sdb/file_test.go
@@ -58,3 +58,136 @@ func TestFileAppendAndReadBackSequentially(t *testing.T) {
 		}
 	}
 }
+
+func TestReadFileSkipSomething(t *testing.T) {
+	partialPaths := []string{troot, "testReadFileSkipSomething"}
+	root := strings.Join(partialPaths, "/")
+
+	os.RemoveAll(root)
+	os.MkdirAll(root, os.ModePerm)
+
+	// prepare a write buffer
+	s := make([]byte, 4096)
+	for i := 0; i < len(s); i++ {
+		res := int8(i % 10)
+		s[i] = uint8('A' + res)
+	}
+
+	// prepare file name
+	fileList := []string{root, "test.txt"}
+	name := strings.Join(fileList, "/")
+
+	{
+		f := MakeLocalWritableFile(name)
+		if f == nil {
+			t.Error("Fails to create writable file ", name)
+		}
+
+		result := f.Append(s)
+		if !result.Ok() {
+			t.Error("Fails to append to a file")
+		}
+
+		result = f.Close()
+		if !result.Ok() {
+			t.Error("Fails to close a file")
+		}
+	}
+
+	{
+		f := MakeLocalSequentialFile(name)
+		if f == nil {
+			t.Error("Fails to open file")
+		}
+
+		defer f.Close()
+
+		f.Skip(58)
+
+		scratch := make([]byte, len(s)-58)
+		res, result := f.Read(uint32(len(s)), scratch)
+
+		if !result.Ok() {
+			t.Error("fails to read from file")
+		}
+
+		if bytes.Compare(s[58:], res) != 0 {
+			t.Error("Read corrupted data")
+		}
+	}
+}
+
+func TestWritableFileMultiAppend(t *testing.T) {
+	partialPaths := []string{troot, "testWritableFileMultiAppend"}
+	root := strings.Join(partialPaths, "/")
+
+	os.RemoveAll(root)
+	os.MkdirAll(root, os.ModePerm)
+
+	// prepare a write buffer
+	s := make([]byte, 4096)
+	for i := 0; i < len(s); i++ {
+		res := int8(i % 10)
+		s[i] = uint8('A' + res)
+	}
+
+	// prepare file name
+	fileList := []string{root, "test.txt"}
+	name := strings.Join(fileList, "/")
+
+	// Append a few bytes
+	{
+		f := MakeLocalWritableFile(name)
+		if f == nil {
+			t.Error("Fails to create writable file ", name)
+		}
+
+		result := f.Append(s[:58])
+		if !result.Ok() {
+			t.Error("Fails to append to a file")
+		}
+
+		result = f.Close()
+		if !result.Ok() {
+			t.Error("Fails to close a file")
+		}
+	}
+
+	// Append more data
+	{
+		f := MakeLocalWritableFile(name)
+		if f == nil {
+			t.Error("Fails to create writable file ", name)
+		}
+
+		result := f.Append(s[58:])
+		if !result.Ok() {
+			t.Error("Fails to append to a file")
+		}
+
+		result = f.Close()
+		if !result.Ok() {
+			t.Error("Fails to close a file")
+		}
+	}
+
+	{
+		f := MakeLocalSequentialFile(name)
+		if f == nil {
+			t.Error("Fails to open file")
+		}
+
+		defer f.Close()
+
+		scratch := make([]byte, len(s))
+		res, result := f.Read(uint32(len(s)), scratch)
+
+		if !result.Ok() {
+			t.Error("fails to read from file")
+		}
+
+		if bytes.Compare(s, res) != 0 {
+			t.Error("Read corrupted data")
+		}
+	}
+}
